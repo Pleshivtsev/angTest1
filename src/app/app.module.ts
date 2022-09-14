1fecf25e316245999b5b7a42eb4c8bc102cea946
Semyon Pleshivtsev <pleshivtsev@gmail.com>
2022-09-14 10:09:11 +0300

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { Comp1Component } from './comp1/comp1.component';
 import { Comp2Component } from './comp2/comp2.component';
 import { Comp3Component } from './comp3/comp3.component';
+import { HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { Comp3Component } from './comp3/comp3.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/country.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CountryService{
+
+  constructor(private http: HttpClient) {
+  }
+
+  searchByCapital(capital: string): Observable<any>{
+    return this.http.get(`https://restcountries.com/v3.1/capital/${capital}`);
+  }
+
+}
+
